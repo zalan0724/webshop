e0327caf361c4bbb8d398b3fb6e046930a2fd3de
zalan0724 <munkacsi0724@gmail.com>
2022-01-01 20:58:05 +0100

    Fix firebase error

--- utils/db/index.js
+++ utils/db/index.js
@@ -1,24 +1,16 @@
 import admin from 'firebase-admin';
 
-const serviceAccount = {
-    type: process.env.FIREBASE_type,
-    project_id: process.env.FIREBASE_project_id,
-    private_key_id: process.env.FIREBASE_private_key_id,
-    private_key: process.env.FIREBASE_private_key,
-    client_email: process.env.FIREBASE_client_email,
-    client_id: process.env.FIREBASE_client_id,
-    auth_uri: process.env.FIREBASE_auth_uri,
-    token_uri: process.env.FIREBASE_token_uri,
-    auth_provider_x509_cert_url:
-        process.env.FIREBASE_auth_provider_x509_cert_url,
-    client_x509_cert_url: process.env.FIREBASE_client_x509_cert_url,
-};
+const app =
+    global.firebaseApp ??
+    admin.initializeApp({
+        credential: admin.credential.cert({
+            projectId: process.env.FIREBASE_project_id,
+            clientEmail: process.env.FIREBASE_client_email,
+            privateKey: process.env.FIREBASE_private_key?.replace(/\\n/g, '\n'),
+        }),
+    });
 
-const app = admin.apps.length
-    ? admin.app
-    : admin.initializeApp({
-          credential: admin.credential.cert(JSON.stringify(serviceAccount)),
-      });
+global.firebaseApp = app;
 
 const db = admin.firestore();
 
