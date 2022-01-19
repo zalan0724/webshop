e22e30a03ea97f5b06281d96816f2c4879516d9e
zalan0724 <munkacsi0724@gmail.com>
2022-01-19 11:57:38 +0100

    Fixing detailed image scaling bug

--- pages/details/[...product].js
+++ pages/details/[...product].js
@@ -3,6 +3,7 @@ import { useRouter } from 'next/router';
 import useSWR from 'swr';
 import axios from 'axios';
 import Image from 'next/image';
+import Head from 'next/head';
 import getProductParams from '../../components/productParams';
 import Details from '../../components/detailsPage/Details';
 import {
@@ -28,71 +29,87 @@ function Product() {
     );
 
     return (
-        <div
-            className={
-                'grid md:grid-cols-3 grid-rows-2 md:grid-rows-1 detailed-bg md:h-screen w-screen box-border px-6 md:px-12 pb-6 md:pb-12 pt-24 relative gap-8'
-            }>
-            {data && (
-                <>
-                    <div
-                        className={
-                            'flex justify-center items-center h-full md:col-span-2 p-6'
-                        }>
+        <>
+            <Head>
+                <title>{data ? data.brand + ' ' + data.name : 'Loading'}</title>
+                <meta name="theme-color" content="#f5f5f4" />
+            </Head>
+            <div
+                className={
+                    'grid md:grid-cols-3 grid-rows-2 md:grid-rows-1 detailed-bg md:h-screen w-screen box-border px-6 md:px-12 pb-6 md:pb-12 pt-24 relative gap-8'
+                }>
+                {data && (
+                    <>
                         <div
                             className={
-                                'relative h-4/5 hover:h-full duration-200 aspect-square'
+                                'flex justify-center items-center h-full md:col-span-2 p-6'
                             }>
-                            <Image
-                                src={data.link}
-                                layout={'fill'}
-                                quality={100}
-                                priority={true}
+                            <div
+                                className={
+                                    'relative h-4/5 md:hover:h-full duration-200 aspect-square'
+                                }>
+                                <Image
+                                    src={data.link}
+                                    layout={'fill'}
+                                    quality={100}
+                                    priority={true}
+                                />
+                            </div>
+                        </div>
+                        <div className={'flex flex-col justify-between'}>
+                            <Details
+                                product={getProductParams(
+                                    data.productType,
+                                    data
+                                )}
                             />
-                        </div>
-                    </div>
-                    <div className={'flex flex-col justify-between'}>
-                        <Details
-                            product={getProductParams(data.productType, data)}
-                        />
-                        <div className={'grid grid-cols-4 gap-4 w-full mt-4'}>
-                            <button
+                            <div
                                 className={
-                                    'col-span-2 text-2xl pr-2 border-stone-300 border-r'
-                                }
-                                onClick={() =>
-                                    addProductToCart(session, data, dispatch)
-                                }
-                                title={'Add to cart'}>
-                                ADD TO CART
-                            </button>
-                            <button
-                                className={
-                                    'flex justify-center items-center col-span-1 pr-2 border-stone-300 border-r'
-                                }
-                                onClick={() =>
-                                    addProductToCompare(data, dispatch)
-                                }
-                                title={'Add to compare'}>
-                                <CompareArrowsOutlinedIcon
-                                    className={'text-4xl text-slate-900'}
-                                />
-                            </button>
-                            <button
-                                className={
-                                    'flex justify-center items-center col-span-1'
-                                }
-                                title={'Add to PC Builder'}>
-                                <DesktopWindowsOutlinedIcon
+                                    'grid grid-cols-4 gap-4 w-full mt-4'
+                                }>
+                                <button
                                     className={
-                                        'text-4xl text-slate-900 opacity-25'
+                                        'col-span-2 text-2xl pr-2 border-stone-300 border-r'
                                     }
-                                />
-                            </button>
+                                    onClick={() =>
+                                        addProductToCart(
+                                            session,
+                                            data,
+                                            dispatch
+                                        )
+                                    }
+                                    title={'Add to cart'}>
+                                    ADD TO CART
+                                </button>
+                                <button
+                                    className={
+                                        'flex justify-center items-center col-span-1 pr-2 border-stone-300 border-r'
+                                    }
+                                    onClick={() =>
+                                        addProductToCompare(data, dispatch)
+                                    }
+                                    title={'Add to compare'}>
+                                    <CompareArrowsOutlinedIcon
+                                        className={'text-4xl text-slate-900'}
+                                    />
+                                </button>
+                                <button
+                                    className={
+                                        'flex justify-center items-center col-span-1'
+                                    }
+                                    title={'Add to PC Builder'}>
+                                    <DesktopWindowsOutlinedIcon
+                                        className={
+                                            'text-4xl text-slate-900 opacity-25'
+                                        }
+                                    />
+                                </button>
+                            </div>
                         </div>
-                    </div>
-                </>
-            )}
-        </div>
+                    </>
+                )}
+            </div>
+        </>
     );
 }
 
